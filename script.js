926647eabd506ca1c73c220d019c87bc850286f6
Sam.O <samovertonjr@gmail.com>
2018-03-21 00:04:31 -0500

    completed quiz app

---
+++ script.js
@@ -0,0 +1,137 @@
+
+// Project
+
+/*
+--- Build a quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function() {
+  
+  function Question(question, answers, correctAnswer) {
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  };
+
+  Question.prototype = {
+
+    constructor: Question,
+
+    showRandomAnswers: function() {
+      
+      console.log(this.question);
+  
+      this.answers.forEach(function(element, index) {
+        console.log(index + ': '+ element);
+      });
+    },
+  
+    showUserAnswer: function(theUserAnswer, callback) {
+      var score;
+
+      if(theUserAnswer === this.correctAnswer) {        
+        console.log('Correct');
+        score = callback(true);
+        
+  
+      } else {
+        console.log('Try again');
+
+        score = callback(false);
+      } 
+
+      this.displayScore(score);
+    },
+
+    displayScore: function(score) {
+      console.log('Your current score is: ' + score);
+      console.log('-------------------------------------');
+    }
+
+  }
+
+ 
+  var blackPanther = new Question();
+  blackPanther.question = 'Where is the Black Panther home country?';
+  blackPanther.answers = ['Africa', 'Wakanda', 'Ghana', 'Egypt'];
+  blackPanther.correctAnswer = 1;
+
+  
+  var spiderman = new Question();
+  spiderman.question = 'Where is Spiderman home city?';
+  spiderman.answers = ['New Jersey', 'Boston', 'New York', 'Milwaukee'];
+  spiderman.correctAnswer = 2;
+
+
+  var batman = new Question();
+  batman.question = 'Where is Batman home city?';
+  batman.answers = ['New York', 'Chicago', 'Metropolis', 'Gotham'];
+  batman.correctAnswer = 3;
+
+
+
+  
+  var questionsArr = [blackPanther, spiderman, batman];
+
+  function theScore() {
+    var score = 0;
+
+    return function(correct) {
+
+      if(correct) {
+        score++
+      }
+      return score;
+    }
+  }
+
+  var keepScore = theScore();
+
+  function nextQuestion() {
+      
+      var randomQuestion = questionsArr[Math.floor(Math.random() * questionsArr.length)];
+
+      randomQuestion.showRandomAnswers();
+      
+      var userAnswer = prompt('Which is the correct answer?');
+
+      if(userAnswer !== 'exit') {
+        randomQuestion.showUserAnswer(parseInt(userAnswer), keepScore);
+
+        nextQuestion();
+
+      } else {
+        
+        console.log('Game Has Ended!');
+      }  
+  }
+
+  nextQuestion();
+
+}());
